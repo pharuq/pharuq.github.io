9ba3d0a065593824d27c2fb941b53b29986f3fae
Ryoma Shindo <ryoma.shindo@repro.io>
2022-09-21 12:20:34 +0900

    change sw

--- site-sw.js
+++
@@ -1,3 +0,0 @@
-console.log('hello from site-sw.js')
-typeof importScripts === "function" && importScripts('https://s3-ap-northeast-1.amazonaws.com/ryoma.shindo-test-backet/repro-sw.min.js');
-// typeof importScripts === "function" && importScripts('https://web-sdk-test-fox-2.s3-ap-northeast-1.amazonaws.com/repro-sw.js');
