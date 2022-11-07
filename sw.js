b78a0c7591414aec78212be33f2668c309929c6c
Ryoma Shindo <ryoma.shindo@repro.io>
2022-11-07 17:27:02 +0900

    a

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// typeof importScripts === "function" && importScripts('https://s3.ap-northeast-1.amazonaws.com/ryoma.shindo-test-backet/repro-sw.min.js');
-typeof importScripts === "function" && importScripts('https://cdn.reproio.com/web/poc/qa/repro-sw.min.js');
+typeof importScripts === "function" && importScripts('https://s3.ap-northeast-1.amazonaws.com/ryoma.shindo-test-backet/repro-sw.min.js');
+// typeof importScripts === "function" && importScripts('https://cdn.reproio.com/web/poc/qa/repro-sw.min.js');
 
