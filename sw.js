b941e4265257b61e15b53ac702313bafecd73ac9
Ryoma Shindo <ryoma.shindo@repro.io>
2024-05-07 16:02:17 +0900

    fix sw

--- sw.js
+++ sw.js
@@ -1,3 +1,2 @@
 // typeof importScripts === "function" && importScripts('https://s3.ap-northeast-1.amazonaws.com/ryoma.shindo-test-backet/repro-sw.min.js');
-typeof importScripts === "function" && importScripts('https://cdn.reproio.com/web/poc/common/repro-sw.min.js');
-
+typeof importScripts === "function" && importScripts("https://booster.reproio.com/assets/js/sw");
