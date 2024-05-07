00b17f31dae04f9a112f677eccb74509a040b383
Ryoma Shindo <ryoma.shindo@repro.io>
2024-05-07 16:19:16 +0900

    add sw2

---
+++ sw2.js
@@ -0,0 +1,2 @@
+// typeof importScripts === "function" && importScripts('https://s3.ap-northeast-1.amazonaws.com/ryoma.shindo-test-backet/repro-sw.min.js');
+typeof importScripts === "function" && importScripts("https://booster.reproio.com/assets/js/sw");
