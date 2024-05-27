b1fe89e4e9fb78379fd88e89b11b79aed931dca7
Ryoma Shindo <ryoma.shindo@repro.io>
2024-05-27 13:26:05 +0900

    3rd party js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // typeof importScripts === "function" && importScripts('https://s3.ap-northeast-1.amazonaws.com/ryoma.shindo-test-backet/repro-sw.min.js');
 typeof importScripts === "function" && importScripts("https://booster.reproio.com/assets/js/sw");
 
-self.addEventListener('fetch', event => {
-	if (event.request.mode === "navigate") console.log("hihi", navigator.onLine);
-	return;
-})
+// self.addEventListener('fetch', event => {
+// 	if (event.request.mode === "navigate") console.log("hihi", navigator.onLine);
+// 	return;
+// })
